d1387071d0648289b5f8b4cb2d6a5495e20f58d0
HedaMV <124881306+HedaMV@users.noreply.github.com>
2024-03-01 09:24:49 -0300

    Add files via upload

---
+++ srv/http/ana3c/php/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Variavel</title>
+</head>
+<body>
+    <?php
+    $nome = "Ana Carolina";
+    const SOBRENOME = "Steinbach De Lima ";
+    echo "Meu Nome é: $nome " . SOBRENOME ;
+    const IDADE = "16 ";
+    echo "</br> Minha Idade é: " . IDADE;
+    const ALTURA = "1,68 " ;
+    echo "</br> Minha Altura é: " . ALTURA;
+    const PESO = "54kg ";
+    echo "</br> Meu Peso é: " . PESO;
+         ?>
+</body>
+</html>
--- srv/http/index.php
+++ srv/http/index.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="variavel.css">
-    <title>Variavel</title>
+    <title>aula</title>
 </head>
 <body>
-    <?php
-    $nome = "Ana Carolina";
-    const SOBRENOME = "Steinbach De Lima ";
-    echo "Meu Nome é: $nome " . SOBRENOME ;
-    const IDADE = "16 ";
-    echo "</br> Minha Idade é: " . IDADE;
-    const ALTURA = "1,68 " ;
-    echo "</br> Minha Altura é: " . ALTURA;
-    const PESO = "54kg ";
-    echo "</br> Meu Peso é: " . PESO;
-         ?>
+    <h1>Meu primeiro PHP<h1>
+        <?php
+        date_default_timezone_set("America/Sao_Paulo");
+        echo"ola mundo. \u{1F30D} <br>";
+        echo date ("d/M/Y"); 
+        echo"<br>";
+        echo date ("g/i");
+        ?>
 </body>
 </html>
